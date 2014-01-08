c7fc47badb19229f812590711333573f99e3e2f3
Jose Carlos Recuero <jose.recuero@gmail.com>
2014-01-07 18:14:15 -0800

    Add basic structures for Dependency Manager.
    It creates basic structures for base classes to be used in the
    Dependency Manager.

--- js/dependency_manager.js
+++ js/dependency_manager.js
@@ -2,14 +2,138 @@ window.onload = function() {
     console.log("Dependency Manager");
 }
 
-function DependencyManager() {};
-
-DependencyManager.prototype.init = function() {
-    return 'init';
+// empty_obj method
+// returns true if object does not have any attribute.
+function empty_obj(obj) {
+    var name;
+    for (name in obj) {
+        return false;
+    }
+    return true;
 };
 
-DependencyManager.prototype.start = function() {
-    return 'start';
+// DM_InstanceState enum values
+var DM_InstanceState = {
+    NONE:      0,
+    CREATED:   1,
+    PARTIAL:   2,
+    ACTIVE:    3,
+    INACTIVE:  4,
+    DELETED:   5,
+    DESTROYED: 6
 };
 
-depMgr = new DependencyManager();
+// DM_Relation enum values
+var DM_Relation = {
+    NONE:    0,
+    MAIN:    1,
+    PARTIAL: 2
+};
+
+// Stat Class Methods
+function Stat() {
+    this._counter = 0;
+    this._enable  = false;
+};
+
+Stat.prototype.enable = function() {
+    this._enable = true;
+};
+
+Stat.prototype.disable = function() {
+    this._enable = false;
+};
+
+Stat.prototype.isEnable = function() {
+    return this._enable;
+};
+
+Stat.prototype.inc = function(amount) {
+    this._counter += typeof amount !== 'undefined' ? amount : 1;
+    return this._counter;
+};
+
+Stat.prototype.dec = function(amount) {
+    this._counter -= typeof amount !== 'undefined' ? amount : 1;
+    if(this._counter < 0) {
+        this._counter = 0;
+    }
+    return this._counter;
+};
+
+Stat.prototype.get_counter = function() {
+    return this._counter;
+};
+
+// DM_InstanceDep Class Methods
+function DM_InstanceDep() {
+    this.name     = null;
+    this.attrs    = [];
+    this.relation = DM_Relation.NONE;
+};
+
+DM_InstanceDep.prototype.create = function(name, attrs, relation) {
+    this.name     = name;
+    this.attrs    = attrs;
+    this.relation = relation;
+    return true;
+}
+
+// DM_Dep Class Methods
+function DM_Dep() {
+    this.name      = null;
+    this.id        = null;
+    this.prio      = null;
+    this.deps      = {};
+    this.callbacks = {};
+};
+
+DM_Dep.prototype.register = function(name, id, prio, deps, callbacks) {
+    this.name      = name;
+    this.id        = id;
+    this.prio      = prio;
+    this.deps      = deps;
+    this.callbacks = callbacks;
+    return true;
+};
+
+// DM_Instance Class Methods
+function DM_Instance() {
+    this.name    = null;
+    this.state   = DM_InstanceState.NONE;
+    this.deps    = null;
+    this.in_deps = null;
+};
+
+DM_Instance.prototype.ID = 0;
+
+DM_Instance.prototype.cleanID = function() {
+    DM_Instance.prototype.ID = 0;
+    return true;
+};
+
+DM_Instance.prototype.register = function(name) {
+    this.name = name;
+    this.state = DM_InstanceState.CREATED;
+    DM_Instance.prototype.ID++;
+    return true;
+};
+
+// DependencyManager Class Methods
+// The First step for any instance is register to the dependency
+// manager, so it will create a new DM_Instance for tracking
+// purposes.
+function DependencyManager() {
+    this.deps = {};
+};
+
+DependencyManager.prototype.register = function(name) {
+    if(this.deps.hasOwnProperty(name)) {
+        return false;
+    } else {
+        var instance = new DM_Instance()
+        this.deps[name] = instance.register(name);
+        return true;
+    }
+};
+
--- test/spec/spec_dependency_manager.js
+++ test/spec/spec_dependency_manager.js
@@ -1,8 +1,279 @@
-describe("Dependency Manager", function() {
-    it("init returns", function() {
-        expect(depMgr.init()).toEqual('init');
+describe("Stat", function() {
+    var stat;
+
+    beforeEach(function() {
+        stat = new Stat();
     });
-    it("start returns", function() {
-        expect(depMgr.start()).toEqual('start');
+
+    afterEach(function() {
+        stat = null;
+    });
+
+    it("new stat has counter to zero", function() {
+        expect(stat._counter).toEqual(0);
+    });
+    it("new stat is disabled", function() {
+        expect(stat._enable).toBe(false);
+    });
+    it("enable stat set enable to true", function() {
+        stat.enable();
+        expect(stat._enable).toBe(true);
+    })
+    it("disable stat set enable to false", function() {
+        stat.enable();
+        expect(stat._enable).toBe(true);
+        stat.disable();
+        expect(stat._enable).toBe(false);
+    });
+    it("isEnable return if enable is true", function() {
+        expect(stat.isEnable()).toBe(false);
+        stat.enable();
+        expect(stat.isEnable()).toBe(true);
+    });
+    it("getCounter returns counter value", function() {
+        expect(stat.get_counter()).toEqual(0);
+        stat._counter = 10;
+        expect(stat.get_counter()).toEqual(10);
+    });
+    it("inc method increase counter value by amount (default is 1)", function() {
+        expect(stat.get_counter()).toEqual(0);
+        expect(stat.inc()).toEqual(1);
+        expect(stat.get_counter()).toEqual(1);
+        expect(stat.inc(10)).toEqual(11);
+        expect(stat.get_counter()).toEqual(11);
+    });
+    it("dec method decrease counter.", function() {
+        expect(stat.get_counter()).toEqual(0);
+        expect(stat.inc()).toEqual(1);
+        expect(stat.get_counter()).toEqual(1);
+        expect(stat.dec()).toEqual(0);
+        expect(stat.get_counter()).toEqual(0);
+        expect(stat.inc(10)).toEqual(10);
+        expect(stat.get_counter()).toEqual(10);
+        expect(stat.dec()).toEqual(9);
+        expect(stat.get_counter()).toEqual(9);
+        expect(stat.dec(5)).toEqual(4);
+        expect(stat.get_counter()).toEqual(4);
+        expect(stat.dec(4)).toEqual(0);
+        expect(stat.get_counter()).toEqual(0);
+    });
+    it("dec can not decrease under zero", function() {
+        expect(stat.dec()).toEqual(0);
+        expect(stat.get_counter()).toEqual(0);
+        expect(stat.inc()).toEqual(1);
+        expect(stat.get_counter()).toEqual(1);
+        expect(stat.dec(10)).toEqual(0);
+        expect(stat.get_counter()).toEqual(0);
     });
 });
+
+describe("Dependency Manager Dependecy Instance", function() {
+    var instance;
+
+    beforeEach(function() {
+        instance = new DM_InstanceDep();
+    });
+
+    afterEach(function() {
+        instance = null;
+    });
+
+    describe("New Instance", function() {
+        it("name is null", function() {
+            expect(instance.name).toBe(null);
+        });
+        it("attrs is empty", function() {
+            expect(instance.attrs.length).toBe(0);
+        });
+        it("relation is NONE", function() {
+            expect(instance.relation).toEqual(DM_Relation.NONE);
+        });
+    });
+
+    describe("Create New Instance Dependency", function() {
+
+        var reto;
+        var name     = "new instance dependency";
+        var attrs    = [1, 2, 3];
+        var relation = DM_Relation.MAIN;
+
+        beforeEach(function() {
+            reto = instance.create(name, attrs, relation);
+        });
+
+        it("Register call returns true", function() {
+            expect(reto).toBe(true);
+        });
+        it("name is 'new instance dependency'", function() {
+            expect(instance.name).toEqual(name);
+        });
+        it("attrs is [1, 2, 3]", function() {
+            expect(instance.attrs).toEqual(attrs);
+        });
+        it("relation is MAIN", function() {
+            expect(instance.relation).toEqual(DM_Relation.MAIN);
+        });
+    });
+});
+
+describe("Dependency Manager Dependency", function() {
+    var dependency;
+
+    beforeEach(function() {
+        dependency = new DM_Dep();
+    });
+
+    afterEach(function() {
+        dependency = null;
+    });
+
+    describe("Create New Dependency", function() {
+        it("name is null", function() {
+            expect(dependency.name).toBe(null);
+        });
+        it("id is null", function() {
+            expect(dependency.id).toBe(null);
+        });
+        it("prio is null", function() {
+            expect(dependency.prio).toBe(null);
+        });
+        it("deps to be empty", function() {
+            expect(empty_obj(dependency.deps)).toBe(true);
+        });
+        it("callbacks to be empty", function() {
+            expect(empty_obj(dependency.callbacks)).toBe(true);
+        });
+    });
+
+    describe("Register Dependency", function() {
+
+        var reto;
+        var name = "new dependency";
+        var id   = "new id";
+        var prio = 100;
+        var deps = {one:1, two:2, three:3};
+        var callbacks = {one: "one", two: "two", three: "three"};
+
+        beforeEach(function() {
+            reto = dependency.register(name, id, prio, deps, callbacks);
+        })
+
+        it("Register call returns true", function() {
+            expect(reto).toBe(true);
+        });
+        it("name is 'new dependency'", function() {
+            expect(dependency.name).toEqual(name);
+        });
+        it("id is 'new id'", function() {
+            expect(dependency.id).toEqual(id);
+        });
+        it("prio is 100", function() {
+            expect(dependency.prio).toEqual(100);
+        });
+        it("deps to be {one:1, two:2, three:3}", function() {
+            expect(dependency.deps).toEqual(deps);
+        });
+        it("callbacks to be {one:'one', two:'two', three:'three'}", function() {
+            expect(dependency.callbacks).toEqual(callbacks);
+        });
+    });
+});
+
+describe("Dependency Manager Instance", function() {
+
+    var instance;
+
+    beforeEach(function() {
+        instance = new DM_Instance();
+    });
+
+    afterEach(function() {
+        instance = null;
+    })
+
+    describe("Create New instance", function() {
+        it("name is null", function() {
+            expect(instance.name).toBe(null);
+        });
+        it("state is NONE", function() {
+            expect(instance.state).toBe(DM_InstanceState.NONE);
+        });
+        it("dependencies are null", function() {
+            expect(instance.deps).toBe(null);
+        });
+        it("in dependencies are null", function() {
+            expect(instance.in_deps).toBe(null);
+        });
+        it("ID is 0", function() {
+            expect(instance.ID).toEqual(0);
+        });
+    });
+
+    describe("Register instance", function() {
+
+        var reto; 
+
+        beforeEach(function() {
+            reto = instance.register('tracing');
+        });
+
+        it("instance name: 'tracing', state: CREATED, ID: 1", function() {
+            expect(reto).toBe(true);
+            expect(instance.name).toEqual('tracing');
+            expect(instance.state).toEqual(DM_InstanceState.CREATED);
+            expect(instance.ID).toEqual(1);
+        })
+    });
+
+    describe("Reset ID", function() {
+        it("ID is reseted to 0", function() {
+            expect(instance.cleanID()).toBe(true);
+            expect(instance.ID).toEqual(0);
+        });
+    });
+});
+
+describe("Dependency Manager", function() {
+
+    var depMgr;
+
+    beforeEach(function() {
+        depMgr = new DependencyManager();
+    });
+
+    afterEach(function() {
+        depMgr = null;
+        DM_Instance.prototype.cleanID();
+    })
+
+    describe("Create New", function() {
+        it("dependencies are empty", function() {
+            expect(empty_obj(depMgr.deps)).toBe(true);
+        });
+    });
+
+    describe("Register Instance", function() {
+
+        var reto;
+        var inst_name = "New Instance";
+
+        beforeEach(function() {
+            reto = depMgr.register(inst_name);
+        });
+
+        it("Register a valid name", function() {
+            expect(reto).toBe(true);
+        });
+
+        it("Dependencies are not more empty", function() {
+            expect(empty_obj(depMgr.deps)).toBe(false);
+            expect(DM_Instance.prototype.ID).toEqual(1);
+        });
+
+        it("Register an existant name", function() {
+            expect(depMgr.register(inst_name)).toBe(false);
+            expect(DM_Instance.prototype.ID).toEqual(1);
+        });
+    });
+});
+
